bc5ed1b081f0bf325de76b4d4219f788639df7ae
muhammedshamil8 <muhammedshamil008@gmail.com>
2025-11-21 07:23:18 +0530

    #

--- src/components/page/LoginPage/LoginForm.tsx
+++ src/components/page/LoginPage/LoginForm.tsx
@@ -23,10 +23,12 @@ export default function LoginForm() {
   const [form] = Form.useForm(); // <-- IMPORTANT
 
   useEffect(() => {
-    supabaseClient().auth.getUser().then(({ data }) => {
-      if (data.user) router.push("/courses");
-      if (statusMessage === "401") msg.info("Please log in first");
-    });
+    supabaseClient()
+      .auth.getUser()
+      .then(({ data }) => {
+        if (data.user) router.push("/courses");
+        if (statusMessage === "401") msg.info("Please log in first");
+      });
   }, []);
 
   const onFinish = async ({ email, password }: LoginFormValues) => {
@@ -52,20 +54,7 @@ export default function LoginForm() {
       <Card className={styles.card}>
         <div className={styles.header}>
           <Title level={3}>Elevate LMS</Title>
-           <div className={styles.welcome}>
-            <Text type="secondary">Welcome back</Text>
-
-            <Button
-              onClick={() =>
-                form.setFieldsValue({
-                  email: "dev@gmail.com",
-                  password: "password",
-                })
-              }
-            >
-              Auto-fill Demo Credentials
-            </Button>
-          </div>
+          <Text type="secondary">Welcome back</Text>
         </div>
 
         {contextHolder}
@@ -87,7 +76,13 @@ export default function LoginForm() {
             <Input.Password size="large" placeholder="••••••••" />
           </Form.Item>
 
-          <Button block type="primary" size="large" htmlType="submit" loading={loading}>
+          <Button
+            block
+            type="primary"
+            size="large"
+            htmlType="submit"
+            loading={loading}
+          >
             Login
           </Button>
         </Form>
